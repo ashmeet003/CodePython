36349657407086fdec7ba1d994af8e3f3a662e4f
Ashmeet Kaur <ashmeet-kaur1@myseneca.ca>
2025-09-11 10:26:33 -0700

    list a3

--- fromCompB10/a3.py
+++ fromCompB10/a3.py
@@ -5,14 +5,10 @@
 liCard = []
 
 # in the following lines, user is asked for input and stored input is appended to the list
-name = input("Enter your full name: ").strip().title() # strip() for extra white space and title() to make 1st letter capital
-liCard.append(name)
-phoneNumber = input("Enter your phone number: ").strip() #strip for extra spaces
-liCard.append(phoneNumber)
-email = input("Enter your email: ").strip().lower() #lower in case capital letters are entered for email
-liCard.append(email)
-birthday = input("Enter your birthday: ").strip().title() # title in case 'aug 8, 2025' is entered
-liCard.append(birthday)
+liCard.append(input("Enter your full name: ").strip().title())  # strip() for extra white space and title() to make 1st letter capital
+liCard.append(input("Enter your phone number: ").strip())       # strip for extra spaces
+liCard.append(input("Enter your email: ").strip().lower())      # lower in case capital letters are entered for email
+liCard.append(input("Enter your birthday: ").strip().title())   # title in case 'aug 8, 2025' is entered
 
 print(liCard) # prints list
 print("\n\n") # adds two line breaks
