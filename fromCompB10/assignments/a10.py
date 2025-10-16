db95763dd10e05d869fee3a0abc650345050e447
Ashmeet Kaur <ashmeet-kaur1@myseneca.ca>
2025-10-16 11:05:40 -0700

    exception handling

--- fromCompB10/assignments/a10.py
+++ fromCompB10/assignments/a10.py
@@ -14,31 +14,56 @@ print("\n")
 #Welcomes user for temperature Calculator
 print("Welcome to Temperature Calculator")
 print("*"*60)
+while True:
+    # asks for user choice between F to C conversion or C to F
+    # converted to int for if-else statement
 
-# asks for user choice between F to C conversion or C to F
-# converted to int for if-else statement
+    # what if user enters a character not int?? ValueError
+    while(True):
+        strOption = input("Enter 1 to choose 'F to C' or 2 for 'C to F': ")
+        if(strOption == "1" or strOption == "2"):
+            intOption = int(strOption)
+            break
+        else:
+            print("Please enter a valid option")
+    print("\n")
 
-# what if user enters a character not int?? Valueerror
-intOption = int(input("Enter 1 to choose 'F to C' or 2 for 'C to F': "))
-print("\n")
+    if intOption == 1:                                                  # if user chooses F to C conversion
 
-if intOption == 1:                                                  # if user chooses F to C conversion
+        # what if user enters a character not float?? ValueError
+        while(True):
+            strTemp = input("Enter temperature in Fahrenheit: ")
+            strNum = strTemp.replace(".","",1)
+            if(strNum.isnumeric()):
+                floatTemp = float(strTemp)   # asks user for temperature, converted to float
+                floatCel = (floatTemp - 32) * 5/9                               # formula for conversion
+                print(f"Temperature in Celsius: {floatCel:.2f}C")               # prints answer
+                break
+            else:
+                print("Please enter a valid Temperature")
 
-    # what if user enters a character not float?? ValueError
-    floatTemp = float(input("Enter temperature in Fahrenheit: "))   # asks user for temperature, converted to float
-    floatCel = (floatTemp - 32) * 5/9                               # formula for conversion
-    print(f"Temperature in Celsius: {floatCel:.2f}C")               # prints answer
+    else:                                                # if user chooses F to C conversion
 
-elif intOption == 2:                                                # if user chooses F to C conversion
+        # what if user enters a character not float?? ValueError
+        while(True):
+            try:
+                floatTemp = float(input("Enter temperature in Celsius: "))      # asks user for temperature, converted to float
+                floatFah = (floatTemp * 9/5) + 32                               # formula for conversion
+                print(f"Temperature in Fahrenheit: {floatFah:.2f}F")            # prints answer
+                break
+            except:
+                print("Please enter a valid Temperature")
 
-    # what if user enters a character not float?? ValueError
-    floatTemp = float(input("Enter temperature in Celsius: "))      # asks user for temperature, converted to float
-    floatFah = (floatTemp * 9/5) + 32                               # formula for conversion
-    print(f"Temperature in Fahrenheit: {floatFah:.2f}F")            # prints answer
+    print("")
+    replay  = input("Do you want to try another temperature conversion? (y/n): ").strip().lower()
+    if(replay == "yes" or replay == "y"):
+        print("")
+        continue
+    elif(replay == "no" or replay == "n"):
+        print("")
+        break
+    else:
+        print("Please enter a valid option from y-yes or n-no")
 
-else:                                                               # if user enters wrong option for conversion
-    print("Invalid input. Please enter 1 or 2")                     # prints 'Invalid choice' message
 
-
-##
-# modify to include option for another calculation
+print("#" * 9, "Thank you for using this program".center(36), "#" * 9)
