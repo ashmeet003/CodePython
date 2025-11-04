41f0d6666a59d2b84475954eb3b95d861cad2adf
Ashmeet Kaur <ashmeet-kaur1@myseneca.ca>
2025-11-04 10:08:16 -0800

    __init__

--- fromCompB10/practiseCode/oopClassEx.py
+++ fromCompB10/practiseCode/oopClassEx.py
@@ -3,6 +3,10 @@ class clCar:
     boolRunning = False
     intMileage = 0
 
+    def __init__(self, strMake = "Toyota", strColor = "White"): # method above any other method, creates new properties
+        self.strMake = strMake
+        self.strColor = strColor
+
     def startCar(self): # this methods belongs to self object
         print("This car has started")
         self.boolRunning = True
@@ -15,8 +19,9 @@ class clCar:
             print("The car needs to be started first")
 
 
-myCar1 = clCar()
-myCar2 = clCar()
+myCar1 = clCar("Toyota Corolla", "Black")
+myCar2 = clCar("Ford", "Green")
+myCar3 = clCar()
 
 print(myCar1.intWheels) #same
 print(myCar2.intWheels) # same
@@ -28,3 +33,6 @@ myCar1.startCar()
 myCar1.driveCar(50)
 myCar2.driveCar(50)
 myCar1.driveCar(50)
+
+print(f"The {myCar1.strMake} is {myCar1.strColor}")
+print(f"The {myCar3.strMake} is {myCar3.strColor}")
