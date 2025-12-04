f9d9523144f8c57f5753fb1c7b31185258ed9505
Ashmeet Kaur <ashmeet-kaur1@myseneca.ca>
2025-12-04 10:47:35 -0800

    tkinter

--- fromCompB10/assignments/a17Tkinter/a17.py
+++ fromCompB10/assignments/a17Tkinter/a17.py
@@ -1,7 +1,81 @@
 from PIL import Image, ImageTk, ImageFilter, ImageEnhance
 import tkinter as tk
 
+def applyFilterOne():
+    global image1, imgDisplay
+    # Split the image into channels
+    imRed, imGreen, imBlue = image1.split()
+    # Rotate the red channel 3 degrees
+    imRed = imRed.rotate(3)
+    # Rotate the green channel 358 degrees
+    # This is the same as 2 degrees CCW
+    imGreen = imGreen.rotate(358)
+    # Blur the blue channel
+    imBlue = imBlue.filter(ImageFilter.GaussianBlur(30))
+    # These two lines adjust the brightness of the blue channel
+    # First, we have to create the enhancer instance
+    enhancer = ImageEnhance.Brightness(imBlue)
+    # Second, we apply it back to the image.
+    imBlue = enhancer.enhance(1.5)
+    # Merge the channels into one RGB image
+    image1 = Image.merge("RGB",(imRed, imGreen, imBlue))
+    # Enhance brightness for the whole image.
+    enhancer = ImageEnhance.Brightness(image1)
+    image1 = enhancer.enhance(1.5)
+    print("\nThe Filter has Been Applied.\n")
+    imgDisplay = ImageTk.PhotoImage(image1)
+    picLabel["image"] = imgDisplay
+
+def applyFilterTwo():
+    global image1, imgDisplay
+    r,g,b = image1.split()
+    # filters red:
+    r = r.filter(ImageFilter.GaussianBlur(30))
+    enhancer = ImageEnhance.Brightness(r)
+    r = enhancer.enhance(0.8)
+    # filters blue:
+    b = b.filter(ImageFilter.EDGE_ENHANCE)
+    enhancer = ImageEnhance.Color(b)
+    b = enhancer.enhance(0.2)
+    # filters green:
+    enhancer = ImageEnhance.Contrast(g)
+    g = enhancer.enhance(2.0)
+    # merges image
+    image1 = Image.merge("RGB",(r,g,b))
+    print("\nThe Filter has Been Applied.\n")
+    # displays and returns image
+    imgDisplay = ImageTk.PhotoImage(image1)
+    picLabel["image"] = imgDisplay
+
+def filterA():
+    global image1, image2, imgDisplay
+    red, green, blue = image2.split()
+    red = red.rotate(-5).filter(ImageFilter.GaussianBlur(10))
+    green = green.rotate(5).filter(ImageFilter.GaussianBlur(5))
+    enh = ImageEnhance.Brightness(blue)
+    blue = enh.enhance(3)
+    image2 = Image.merge("RGB", (red, green, blue))
+    imgDisplay = ImageTk.PhotoImage(image2)
+    picLabel["image"] = imgDisplay
+
 root = tk.Tk()
 root.title("Filter Image App")
 root.geometry("1000x600")
+
+
+filename = "images/beach.jpg"
+image1 = Image.open(filename)
+
+imgDisplay = ImageTk.PhotoImage(image1)
+
+txtLabel = tk.Label(root,text="Picture!")
+txtLabel.pack()
+picLabel = tk.Label(root, image=imgDisplay, width=550, height=350)
+picLabel.pack()
+
+
+filter1Button = tk.Button(master = root, text = "Filter 1", command = applyFilterOne)
+filter1Button.pack()
+filter2Button = tk.Button(master = root, text = "Filter 2", command = applyFilterTwo)
+filter2Button.pack()
 root.mainloop()
---
+++ fromCompB10/practiseCode/pyGame/gameAsteroid.py
@@ -0,0 +1,164 @@
+# Import the pygame module
+import pygame
+import random
+
+# Import pygame.locals for easier access to key coordinates
+from pygame.locals import (
+    RLEACCEL,
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+# Define constants for the screen width and height
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+
+
+# Define a player object by extending pygame.sprite.Sprite
+# The surface drawn on the screen is now an attribute of 'player'
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.image.load("../pygame_images/jet.png").convert()
+        self.surf.set_colorkey((255, 255, 255), RLEACCEL)
+        self.rect = self.surf.get_rect()
+        self.rect.top = 250
+
+    # Move the sprite based on user keypresses
+    def update(self, pressed_keys):
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -5)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 5)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-5, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(5, 0)
+
+        # Keep player on the screen
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+
+# Define the enemy object by extending pygame.sprite.Sprite
+# The surface you draw on the screen is now an attribute of 'enemy'
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        global score
+        super(Enemy, self).__init__()
+        food=["burger","pizza", "fries"]
+        self.surf = pygame.image.load("../pygame_images/missile.png").convert()
+        self.surf.set_colorkey((255, 255, 255), RLEACCEL)
+        self.rect = self.surf.get_rect(
+            center=(
+                random.randint(0, SCREEN_WIDTH), #x coordinates
+                random.randint(-100, -20), # y coordinates
+            )
+        )
+        # Use the score to increase difficulty over time
+        slow = 0 + score//15
+        fast = 2 + score // 10
+        self.speed = random.randint(slow, fast)
+
+    # Move the sprite based on speed
+    # Remove the sprite when it passes the left edge of the screen
+    def update(self):
+        global score
+        self.rect.move_ip(0,self.speed)
+        if self.rect.right < 0:
+            self.kill()
+            score += 1
+
+# Initialize pygame
+pygame.init()
+
+score = 0
+SCORE_FONT = pygame.font.Font(None, 36)
+# Create the screen object
+# The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# Create a custom event for adding a new enemy
+ADDENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(ADDENEMY, 500-(score*2))
+
+# Instantiate player.
+player = Player()
+
+# Create groups to hold enemy sprites and all sprites
+# - enemies is used for collision detection and position updates
+enemies = pygame.sprite.Group()
+# - all_sprites is used for rendering everything
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+
+# Variable to keep the main loop running
+running = True
+
+# Setup the clock for a decent framerate
+clock = pygame.time.Clock()
+
+
+def draw_score():
+    score_surface = SCORE_FONT.render(f"Score: {score}", True, (255, 255, 255))
+    screen.blit(score_surface, (10, 10))
+
+# Main loop
+while running:
+    # for loop through the event queue
+    for event in pygame.event.get():
+        # Check for KEYDOWN event
+        if event.type == KEYDOWN:
+            # If the Esc key is pressed, then exit the main loop
+            if event.key == K_ESCAPE:
+                running = False
+        # Check for QUIT event. If QUIT, then set running to false.
+        elif event.type == QUIT:
+            running = False
+        # Add a new enemy?
+        elif event.type == ADDENEMY:
+            # Create the new enemy and add it to sprite groups
+            new_enemy = Enemy()
+            enemies.add(new_enemy)
+            all_sprites.add(new_enemy)
+
+    # Get the set of keys pressed and check for user input
+    pressed_keys = pygame.key.get_pressed()
+    #update player
+    player.update(pressed_keys)
+
+    # Update enemy position
+    enemies.update()
+
+
+    # Fill the screen color
+    screen.fill((23,183,232))
+
+    # Draw all sprites
+    for entity in all_sprites:
+        screen.blit(entity.surf, entity.rect)
+
+    draw_score()
+
+    # Check if any enemies have collided with the player
+    if pygame.sprite.spritecollideany(player, enemies):
+        # If so, then remove the player and stop the loop
+        player.kill()
+        running = False
+
+    # Update the display
+    pygame.display.flip()
+
+    # Ensure program maintains a rate of 30 frames per second
+    clock.tick(60)
